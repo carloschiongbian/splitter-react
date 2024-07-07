f77e7a6663017d30b83b7acb5345b83446ca9218
Carlos Julian H. Chiongbian <carlos.chiongbian.lanexcorp.com>
2024-07-07 10:18:00 +0800

    fix: resolved NaN display

--- src/public/js/CalculateFunctions.js
+++ src/public/js/CalculateFunctions.js
@@ -1,36 +1,38 @@
-import '../../components/TotalAmounts';
+import "../../components/TotalAmounts";
 
 const CalculateFunctions = () => {
+  const isValid = (percentage) => {
+    var billAmount = Number(document.getElementById("bill-amount").value);
+    var noOfPeople = Number(document.getElementById("no-of-people").value);
 
-    const isValid = (percentage) => {
-        var billAmount = document.getElementById("bill-amount").value;
-        var noOfPeople = document.getElementById("no-of-people").value;
-        
-        if(noOfPeople === "0") {
-            console.log("no-of-people warning");
-            document.getElementById("no-of-people").style.border = "3px solid red";
-            document.getElementById("no-of-people-warning").style.display = "block";
+    if (noOfPeople === 0) {
+      document.getElementById("no-of-people").style.border = "3px solid red";
+      document.getElementById("no-of-people-warning").style.display = "block";
+    } else {
+      document.getElementById("no-of-people").style.border = "";
+      document.getElementById("no-of-people-warning").style.display = "none";
+      document.getElementById("reset").style.backgroundColor = "white";
+      document.getElementById("reset").style.cursor = "pointer";
+      Calculate(billAmount, percentage, noOfPeople);
+    }
+  };
 
-        } else {
-            console.log("calculate condition");
-            document.getElementById("no-of-people").style.border = "";
-            document.getElementById("no-of-people-warning").style.display = "none";
-            document.getElementById("reset").style.backgroundColor = "white";
-            document.getElementById("reset").style.cursor = "pointer";
-            Calculate(billAmount, percentage, noOfPeople);
-
-        } 
+  const Calculate = (billAmount, percentage, noOfPeople) => {
+    if (isNaN(billAmount) || billAmount === 0) {
+      document.getElementById("total-amount").innerText = `$${(0).toFixed(2)}`;
+      return;
     }
 
-    const Calculate = (billAmount, percentage, noOfPeople) => {
-        var tipAmountPerPerson = billAmount * percentage;
-        document.getElementById("tip-amount-per-person").innerText = "$" + (parseInt( tipAmountPerPerson * 100 ) / 100).toFixed(2);
+    var tipAmountPerPerson = billAmount * percentage;
+    document.getElementById("tip-amount-per-person").innerText =
+      "$" + ((Number(tipAmountPerPerson) * 100) / 100).toFixed(2);
 
-        var totalTipAmount = tipAmountPerPerson / noOfPeople;
-        document.getElementById("total-amount").innerText = "$" + (parseInt( totalTipAmount * 100 ) / 100).toFixed(2);
-    }
+    var totalTipAmount = tipAmountPerPerson / noOfPeople;
+    document.getElementById("total-amount").innerText =
+      "$" + ((Number(totalTipAmount) * 100) / 100).toFixed(2);
+  };
 
-    return { isValid }
-}
+  return { isValid };
+};
 
 export default CalculateFunctions;
